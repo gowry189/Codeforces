cda04bc8920a137ef9edc5667c310d362195ab6b
gowry189 <gowry18@gmail.com>
2018-06-05 15:10:51 +0530

    codeforces

---
+++ watermelon.c
@@ -0,0 +1,17 @@
+/*to print yes if the numbers after dividing a particular number is even*/ 
+
+#include<stdio.h>
+int main()
+{
+    int w;
+    scanf("%d",&w);
+    {
+    if (w==2)
+    printf("NO");
+    else if (w%2==0)
+    printf("YES");
+    else
+    printf("NO");
+    }
+    return 0;
+}
